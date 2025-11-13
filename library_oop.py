191468c31810a3d2f517aeb066163d6a4c47daa5
J <p.niyathirakul@gmail.com>
2025-11-13 22:52:53 +0700

    Add Member class and test its methods

--- library_oop.py
+++ library_oop.py
@@ -17,3 +17,23 @@ class Book:
             self.available_copies = self.available_copies + 1
             return True
         return False
+    
+class Member:
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.borrowed_books = []
+
+    def borrow_book(self, book):
+        if book.borrow():
+            self.borrowed_books.append(book)
+            return True
+        return False
+
+    def return_book(self, book):
+        if book in self.borrowed_books:
+            book.return_book()
+            self.borrowed_books.remove(book)
+            return True
+        return False
